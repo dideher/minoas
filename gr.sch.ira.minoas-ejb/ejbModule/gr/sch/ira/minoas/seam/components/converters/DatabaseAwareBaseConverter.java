b7909c42183e57fc2bc76c33961e9124b039fe37
Filippos Slavik <filippos@slavik.gr>
2008-11-26 08:00:49 +0000

    SEAM 2.1.1CR1 broke converters - it is now required a converter to use the @BypassInterceptors, therefore we need another mean to obtain the reference to the MINOAS database.

--- gr.sch.ira.minoas-ejb/ejbModule/gr/sch/ira/minoas/seam/components/converters/DatabaseAwareBaseConverter.java
+++ gr.sch.ira.minoas-ejb/ejbModule/gr/sch/ira/minoas/seam/components/converters/DatabaseAwareBaseConverter.java
@@ -5,19 +5,24 @@ package gr.sch.ira.minoas.seam.components.converters;
 
 import javax.persistence.EntityManager;
 
-import org.jboss.seam.annotations.In;
+import org.jboss.seam.Component;
 
 /**
  * @author slavikos
  *
  */
+
 public abstract class DatabaseAwareBaseConverter extends BaseConverter {
 	
-	@In
-	private EntityManager minoasDatabase;
-
+	/**
+	 * Comment for <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static final String ENTITY_MANAGER_SEAM_COMPONENT_NAME = "minoasDatabase";
+	
 	protected EntityManager getMinoasDatabase() {
-		return minoasDatabase;
+		return (EntityManager)Component.getInstance(ENTITY_MANAGER_SEAM_COMPONENT_NAME);
 	}
 
 }
