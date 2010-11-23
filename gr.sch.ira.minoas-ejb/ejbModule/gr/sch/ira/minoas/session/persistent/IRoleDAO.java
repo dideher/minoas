80813524c9c5a884cab68b8796a5808a721ab6d4
Filippos Slavik <filippos@slavik.gr>
2010-11-23 10:16:02 +0200

    Removed old projects

--- gr.sch.ira.minoas-ejb/ejbModule/gr/sch/ira/minoas/session/persistent/IRoleDAO.java
+++
@@ -1,7 +0,0 @@
-package gr.sch.ira.minoas.session.persistent;
-
-import gr.sch.ira.minoas.model.security.Role;
-
-public interface IRoleDAO extends IGenericDAO<Role, Long> {
-	public Role findByName(String name);
-}
