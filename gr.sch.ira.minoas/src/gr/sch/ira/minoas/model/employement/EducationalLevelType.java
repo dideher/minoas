23550005ea190ada6be602f25ba99c8bf7daaef3
Yorgos Andreadakis <andreadakis@gmail.com>
2011-06-08 14:13:32 +0300

    Created or changed necessary classes in order to prepare relative objects to suppor the MK functionality.

---
+++ gr.sch.ira.minoas/src/gr/sch/ira/minoas/model/employement/EducationalLevelType.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package gr.sch.ira.minoas.model.employement;
+
+/**
+ * @author <a href="mailto:gand@sch.gr">Yorgos Andreadakis</a>
+ * 
+ */
+public enum EducationalLevelType {
+	/**
+	 * Πανεπιστημιακής Εκπαίδευσης
+	 */
+	UNIVERSITY_EDUCATION_LEVEL("ΠΕ"),
+	/**
+	 * Τεχνολογικής Εκπαίδευσης
+	 */
+	TECHNOLOGIGAL_EDUCATION_LEVEL("ΤΕ"),
+	/**
+	 * Δευτεροβάθμιας Εκπαίδευσης
+	 */
+	SECONDARY_EDUCATION_LEVEL("ΔΕ")
+	;
+
+	private String key;
+
+	private EducationalLevelType(String key) {
+		this.key = key;
+	}
+
+	/**
+	 * @return the key
+	 */
+	public String getKey() {
+		return key;
+	}
+
+	/**
+	 * @param key the key to set
+	 */
+	public void setKey(String key) {
+		this.key = key;
+	}
+}
