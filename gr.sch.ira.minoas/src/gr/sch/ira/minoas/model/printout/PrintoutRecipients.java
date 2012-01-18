edc0d34c2d2823604352341a54fc07bad37e7f6b
Filippos Slavik <filippos@slavik.gr>
2012-01-18 12:28:55 +0200

    Added various printing helper entities

---
+++ gr.sch.ira.minoas/src/gr/sch/ira/minoas/model/printout/PrintoutRecipients.java
@@ -0,0 +1,44 @@
+package gr.sch.ira.minoas.model.printout;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import gr.sch.ira.minoas.model.BaseIDModel;
+
+@Entity
+@Table(name = "PRINTOUT_RECIPIENTS")
+@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+public class PrintoutRecipients extends BaseIDModel {
+    
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = 1L;
+    
+    @Basic
+    @Column(name = "RECIPIENT_TITLE", nullable = false, length = 128)
+    private String recipientTitle;
+
+    /**
+     * @return the recipientTitle
+     */
+    public String getRecipientTitle() {
+        return recipientTitle;
+    }
+
+    /**
+     * @param recipientTitle the recipientTitle to set
+     */
+    public void setRecipientTitle(String recipientTitle) {
+        this.recipientTitle = recipientTitle;
+    }
+    
+    
+   
+        
+}
