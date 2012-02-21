db549e48ffdb203fac8daefc602521fdbebe087b
Filippos Slavik <fsla@forthnet.gr>
2012-02-21 09:40:28 +0200

    gh-40: fixes to leave import scripts

--- sql scripts/importLeaves.sql
+++ sql scripts/importLeaves.sql
@@ -1,3 +1,5 @@
+// sample : EXEC minoas..importEmployeeLeave 420968, 9281
+
 USE minoas
 IF EXISTS (SELECT name FROM sysobjects 
          WHERE name = 'importEmployeeLeave' AND type = 'P')
@@ -20,27 +22,60 @@ DECLARE @legacyNote nvarchar(33)
 DECLARE @legacyCode nvarchar(2)
 DECLARE @minoasLeaveType int;
 DECLARE @minoasLeaveGenerateCDR int;
+SET NOCOUNT ON;
 
-
-SET @oldLeavesCursor = CURSOR FOR SELECT b.APO, b.EVS, b.ARHMER, b.NOTE, b.CODE FROM mkdb..bohu b WHERE b.KVD=@legacyEmployeeID 
+DELETE minoas..EMPLOYEE_LEAVES WHERE EMPLOYEE_ID=@minoasEmployeeID
+SET @oldLeavesCursor = CURSOR FOR SELECT b.APO, b.EVS, b.ARHMER, b.NOTE, b.CODE 
+    FROM mkdb..bohu b 
+WHERE b.KVD=@legacyEmployeeID 
+    ORDER BY b.APO
 
 OPEN @oldLeavesCursor 
 FETCH NEXT FROM @oldLeavesCursor INTO @legacyFrom, @legacyTo, @legacyDayCount, @legacyNote, @legacyCode
 WHILE @@FETCH_STATUS = 0 
 BEGIN 
     // Match minoas leave type from the legacy leave code
-     SELECT @minoasLeaveType=t.ID, @minoasLeaveGenerateCDR=t.GENERATES_CDRS  FROM minoas..EMPLOYEE_LEAVE_TYPE t WHERE t.LEGACY_CODE = @legacyCode
-    IF(@minoasLeaveType= NULL)
+    SELECT @minoasLeaveType=t.ID, @minoasLeaveGenerateCDR=t.GENERATES_CDRS  FROM minoas..EMPLOYEE_LEAVE_TYPE t WHERE t.LEGACY_CODE = @legacyCode
+    IF(@minoasLeaveType IS NOT NULL)
     BEGIN
-        PRINT 'COULD NOT MAP LEAVE TYPE'
-        RETURN
+        INSERT INTO minoas..EMPLOYEE_LEAVES(VERSION, IS_ACTIVE, COMMENT, DUE_TO, ESTABLISHED, EFFECTIVE_DAYS_COUNT, EMPLOYEE_LEAVE_TYPE_ID, EMPLOYEE_ID, GENERATES_CDRS)
+        VALUES(0, 0,@legacyNote, @legacyTo, @legacyFrom, @legacyDayCount, @minoasLeaveType, @minoasEmployeeID, @minoasLeaveGenerateCDR)
     END
-    INSERT INTO minoas..EMPLOYEE_LEAVES(VERSION, IS_ACTIVE, COMMENT, DUE_TO, ESTABLISHED, EFFECTIVE_DAYS_COUNT, EMPLOYEE_LEAVE_TYPE_ID, EMPLOYEE_ID)
-        VALUES(0, 0,@legacyNote, @legacyTo, @legacyFrom, @legacyDayCount, @minoasLeaveType, @minoasEmployeeID)
     FETCH NEXT FROM @oldLeavesCursor INTO @legacyFrom, @legacyTo, @legacyDayCount, @legacyNote, @legacyCode
 END 
 CLOSE @oldLeavesCursor 
 DEALLOCATE @oldLeavesCursor
 
 END 
+
+SELECT * FROM EMPLOYEE e WHERE e.LAST_NAME LIKE 'ΑΝΔΡΕΑΔ%'
+
+exec minoas..importEmployeeLeave 420029, 8879
+
+
+// DO THE IMPORT
+    BEGIN
+        SET NOCOUNT ON;
+        DECLARE @legacyEmployeeID varchar(6);
+        DECLARE @minoasEmployeeID int;
+        DECLARE @cursor CURSOR;
+        DECLARE @RetValue int;
+        SET @cursor = CURSOR FOR SELECT b.KVD,e.ID  FROM minoas..EMPLOYEE e
+            INNER JOIN minoas..REGULAR_EMPLOYEE_INFO i ON i.EMPLOYEE_ID=e.ID
+            INNER JOIN mkdb..basiko b ON LTRIM(RTRIM(b.AM))=i.REGISTRY_ID
+        WHERE e.EMPLOYEE_TYPE='REGULAR'
+
+        OPEN @cursor 
+        FETCH NEXT FROM @cursor INTO @legacyEmployeeID, @minoasEmployeeID;
+        WHILE @@FETCH_STATUS = 0 
+        BEGIN
+            EXEC @RetValue = minoas..importEmployeeLeave @legacyEmployeeID, @minoasEmployeeID
+            FETCH NEXT FROM @cursor INTO @legacyEmployeeID, @minoasEmployeeID;
+        END
+        CLOSE @cursor 
+        DEALLOCATE @cursor
+    END
+
+
+
 
