237dbeee7bf8574b4a859928a199799702e68da6
Yorgos Andreadakis <andreadakis@gmail.com>
2012-10-16 10:43:01 +0300

    Adding xhtml file. Now Work Experience Management is hopefully complete

--- gr.sch.ira.minoas/src/gr/sch/ira/minoas/model/employee/EmployeeInfo.java
+++ gr.sch.ira.minoas/src/gr/sch/ira/minoas/model/employee/EmployeeInfo.java
@@ -165,6 +165,35 @@ public class EmployeeInfo extends BaseIDModel {
 	private SectorType sector;
 
 	
+	/**
+	 * Sum of Educational Experience (Άθροισμα Εκπαιδευτικής Προϋπηρεσίας)
+	 */
+	@Basic
+	@Column(name = "SUM_OF_EDUCATIONAL_EXPERIENCE")
+	private Integer sumOfEducationalExperience;
+	
+	/**
+	 * Sum of Teaching Experience (Άθροισμα Διδακτικής Προϋπηρεσίας)
+	 */
+	@Basic
+	@Column(name = "SUM_OF_TEACHING_EXPERIENCE")
+	private Integer sumOfTeachingExperience;
+	
+	/**
+	 * Sum of Any Type Of Working Experience (Άθροισμα Προϋπηρεσίας Οποιουδήποτε Τύπου)
+	 */
+	@Basic
+	@Column(name = "SUM_OF_EXPERIENCE")
+	private Integer sumOfExperience;
+
+	/**
+	 * Total Work Service in days (Does NOT include prior work experience)
+	 * Συνολική Υπηρεσία από την ημ/νία διορισμού έως σήμερα (δεν συμπεριλαμβάνει προϋπηρεσίες)
+	 */
+	@Basic
+	@Column(name = "TOTAL_WORK_SERVICE")
+	private Integer totalWorkService;
+	
 	
 	/**
 	 * @return the employee
@@ -404,6 +433,62 @@ public class EmployeeInfo extends BaseIDModel {
 
 	public SectorType getSector() {
 		return sector;
+	}
+
+	/**
+	 * @return the sumOfEducationalExperience
+	 */
+	public Integer getSumOfEducationalExperience() {
+		return sumOfEducationalExperience;
+	}
+
+	/**
+	 * @param sumOfEducationalExperience the sumOfEducationalExperience to set
+	 */
+	public void setSumOfEducationalExperience(Integer sumOfEducationalExperience) {
+		this.sumOfEducationalExperience = sumOfEducationalExperience;
+	}
+
+	/**
+	 * @return the sumOfTeachingExperience
+	 */
+	public Integer getSumOfTeachingExperience() {
+		return sumOfTeachingExperience;
+	}
+
+	/**
+	 * @param sumOfTeachingExperience the sumOfTeachingExperience to set
+	 */
+	public void setSumOfTeachingExperience(Integer sumOfTeachingExperience) {
+		this.sumOfTeachingExperience = sumOfTeachingExperience;
+	}
+
+	/**
+	 * @return the sumOfExperience
+	 */
+	public Integer getSumOfExperience() {
+		return sumOfExperience;
+	}
+
+	/**
+	 * @param sumOfExperience the sumOfExperience to set
+	 */
+	public void setSumOfExperience(Integer sumOfExperience) {
+		this.sumOfExperience = sumOfExperience;
+	}
+
+	/**
+	 * @return the totalWorkService
+	 */
+	public Integer getTotalWorkService() {
+		return totalWorkService;
+	}
+
+	/**
+	 * @param totalWorkService the totalWorkService to set
+	 */
+	public void setTotalWorkService(Integer totalWorkService) {
+		this.totalWorkService = totalWorkService;
 	}
 	
 	
