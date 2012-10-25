3b69644fa70d05de42d486deb7ede5f9ea17588d
Filippos Slavik <filippos@slavik.gr>
2012-10-25 14:33:04 +0300

    modularized workExperienceAndServiceInfo.xhtml component so there are no dependencies to EmployeeInfoManagement

---
+++ gr.sch.ira.minoas/src/gr/sch/ira/minoas/seam/components/managers/EmployeeTeachingService.java
@@ -0,0 +1,34 @@
+package gr.sch.ira.minoas.seam.components.managers;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Unwrap;
+
+import gr.sch.ira.minoas.model.employee.EmployeeInfo;
+import gr.sch.ira.minoas.seam.components.BaseSeamComponent;
+import gr.sch.ira.minoas.seam.components.home.EmployeeHome;
+
+@Name("employeeTeachingService")
+public class EmployeeTeachingService extends BaseSeamComponent {
+
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = 1L;
+
+    @In
+    private EmployeeHome employeeHome;
+
+    public EmployeeTeachingService() {
+    }
+
+    @Unwrap
+    public Integer calculate() {
+        EmployeeInfo employeeInfo = employeeHome.getInstance().getEmployeeInfo();
+        if (employeeInfo != null)
+            return  employeeInfo.getSumOfTeachingExperience() + employeeInfo.getTotalWorkService();
+        else
+            return 0;
+    }
+
+}
