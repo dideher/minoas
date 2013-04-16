9d8ec324ba7b817d2882b91928df714d53391461
Filippos Slavik <filippos@slavik.gr>
2013-04-16 14:25:33 +0300

    gh-131 : initial working version

--- gr.sch.ira.minoas/src/main/java/gr/sch/ira/minoas/model/employee/WorkAttendanceEvent.java
+++ gr.sch.ira.minoas/src/main/java/gr/sch/ira/minoas/model/employee/WorkAttendanceEvent.java
@@ -4,7 +4,6 @@
 package gr.sch.ira.minoas.model.employee;
 
 import gr.sch.ira.minoas.model.BaseIDModel;
-import gr.sch.ira.minoas.model.security.Principal;
 
 import java.util.Date;
 
@@ -13,9 +12,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -37,30 +33,40 @@ public class WorkAttendanceEvent extends BaseIDModel {
 	 */
 	private static final long serialVersionUID = 1L;
 
-		
-
 	@Enumerated(EnumType.STRING)
 	@Column(name = "EVENT_TYPE", nullable = false)
 	private WorkAttendanceEventType attendaceType;
-
+	
 	@Basic
 	@Column(name = "COMMENT", nullable = true, length = 255)
 	private String comment;
 
-	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="EVENT_OCCURED_ON", nullable=false)
+	private Date occuredOn;
+
 	public WorkAttendanceEventType getAttendaceType() {
 		return attendaceType;
-	}
-
-	public void setAttendaceType(WorkAttendanceEventType attendaceType) {
-		this.attendaceType = attendaceType;
 	}
 
 	public String getComment() {
 		return comment;
 	}
 
+	
+	public Date getOccuredOn() {
+		return occuredOn;
+	}
+
+	public void setAttendaceType(WorkAttendanceEventType attendaceType) {
+		this.attendaceType = attendaceType;
+	}
+
 	public void setComment(String comment) {
 		this.comment = comment;
+	}
+
+	public void setOccuredOn(Date occuredOn) {
+		this.occuredOn = occuredOn;
 	}
 }
