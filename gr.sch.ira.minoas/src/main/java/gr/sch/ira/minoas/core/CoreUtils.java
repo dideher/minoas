31923259b951c987500560b851d2d35ebc4868a6
Yorgos Andreadakis <andreadakis@gmail.com>
2013-06-21 14:55:23 +0300

    gh-97: Committing 360days year calculation methods

--- gr.sch.ira.minoas/src/main/java/gr/sch/ira/minoas/core/CoreUtils.java
+++ gr.sch.ira.minoas/src/main/java/gr/sch/ira/minoas/core/CoreUtils.java
@@ -72,17 +72,37 @@ public abstract class CoreUtils {
     }
     
     //	Return the days difference between two dates in a 360 days year (12 * 30 days)
+    
+    /**
+     * @param dateFrom: The start date of the period. 
+     * @param dateTo: The end date of the period. 
+     * @return the days difference between two dates in a 360 days year (12 * 30 days). Returns 0 if start date is AFTER end date.
+     */
     public static int datesDifferenceIn360DaysYear(Date dateFrom, Date dateTo) {
     	if(dateFrom == null || dateTo == null || dateFrom.after(dateTo))
+    		return 0;
+        
+    	return get360DaysYearDiff(dateFrom, dateTo);
+    }
+
+    
+    /**
+     * @param dateFrom: The start date of the period. 
+     * @param dateTo: The end date of the period. 
+     * @return the days difference between two dates in a 360 days year (12 * 30 days). Returns NEGATIVE value if start date is AFTER end date.
+     */
+    public static int get360DaysYearDiff(Date dateFrom, Date dateTo) {
+    	if(dateFrom == null || dateTo == null)
     		return 0;
         Calendar startingDate = getStartingDate(dateFrom);
         Calendar endingDate = getEndingDateAccordingToStartingDate(dateTo, startingDate);
         long startingDay = startingDate.get(Calendar.MONTH) * 30 + startingDate.get(Calendar.DAY_OF_MONTH);
         long endingDay = (endingDate.get(Calendar.YEAR) - startingDate.get(Calendar.YEAR)) * 360
                 + endingDate.get(Calendar.MONTH) * 30 + endingDate.get(Calendar.DAY_OF_MONTH);
-        return ((int)(endingDay - startingDay))+1;
+        return ((int)(endingDay - startingDay));
     }
-
+    
+    
     private static Calendar getCalendar(Date date) {
         Calendar processedDate = new GregorianCalendar();
         processedDate.setTime(date);
@@ -157,5 +177,58 @@ public abstract class CoreUtils {
 		return object != null && String.valueOf(object).trim().length() > 0;
 	}
 
+	
+	/**
+	 * @param startDate. The date you want to start counting.
+	 * @param noOfDaysOffset. Positive value will take you to the future. Negative value will take you back in time.
+	 * @return the Calendar date found before/after noOfDaysOffset days. 
+	 */
+	public static Date getDateInXDays(Date startDate, Integer noOfDaysOffset) {
+		return DateUtils.addDays(startDate, noOfDaysOffset);
+	}
 
+	
+	/**
+	 * @param startDate. The date you want to start counting.
+	 * @param noOfDaysOffset. Positive value will take you to the future. Negative value will take you back in time.
+	 * @return the Calendar date found before/after noOfDaysOffset days in a 360 days year. 
+	 */
+	public static Date getDateInXDays360(Date startDate, int noOfDaysOffset) {
+		int efforts = 50;
+		java.util.Calendar estimatedDate = new java.util.GregorianCalendar();
+		estimatedDate.setTime(startDate);
+		
+		if(noOfDaysOffset == 0)
+			return startDate;
+		else	
+		if(noOfDaysOffset < 0) {
+			int xDays = (int) (Math.ceil(noOfDaysOffset*1.015));
+			estimatedDate.setTime(org.apache.commons.lang.time.DateUtils.addDays(estimatedDate.getTime(), xDays));
+
+			for(int effort = 1 ; effort < efforts ; effort++) {
+				int difference  = gr.sch.ira.minoas.core.CoreUtils.get360DaysYearDiff(estimatedDate.getTime(), startDate);
+
+				if(Math.abs(difference) <= Math.abs(noOfDaysOffset)) {
+					if(Math.abs(difference) < Math.abs(noOfDaysOffset))
+						estimatedDate.add(java.util.Calendar.DAY_OF_YEAR, -1);
+					return estimatedDate.getTime();	
+				}
+				estimatedDate.add(java.util.Calendar.DAY_OF_YEAR, 1);
+			}
+		} else {
+			int xDays = (int) (Math.ceil(noOfDaysOffset*1.0145));
+			estimatedDate.setTime(org.apache.commons.lang.time.DateUtils.addDays(estimatedDate.getTime(), xDays));
+
+			for(int effort = 1 ; effort < efforts ; effort++) {
+				int difference  = gr.sch.ira.minoas.core.CoreUtils.get360DaysYearDiff(estimatedDate.getTime(), startDate);
+
+				if(Math.abs(difference) >= Math.abs(noOfDaysOffset)) {
+					return estimatedDate.getTime();	
+				}
+				estimatedDate.add(java.util.Calendar.DAY_OF_YEAR, 1);
+			}
+		}
+
+		return null;
+	}
 }
