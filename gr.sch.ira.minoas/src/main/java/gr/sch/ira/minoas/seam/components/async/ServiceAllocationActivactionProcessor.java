88f0a36824032f0648f7fe3f116a5f34510eaa7a
Filippos Slavik <filippos@slavik.gr>
2013-08-13 15:12:54 +0300

    gh-63 : fixed auto-activation of service allocations

--- gr.sch.ira.minoas/src/main/java/gr/sch/ira/minoas/seam/components/async/ServiceAllocationActivactionProcessor.java
+++ gr.sch.ira.minoas/src/main/java/gr/sch/ira/minoas/seam/components/async/ServiceAllocationActivactionProcessor.java
@@ -49,7 +49,7 @@ public class ServiceAllocationActivactionProcessor extends BaseDatabaseAwareSeam
 	@SuppressWarnings("unchecked")
     @Transactional(TransactionPropagationType.REQUIRED)
     public Collection<ServiceAllocation> getServiceAllocationThatShouldBeActivated(EntityManager em, Date today) {
-        return em.createQuery("SELECT s from ServiceAllocation s WHERE s.active IS FALSE AND (s.autoCanceled IS FALSE OR s.autoCanceled IS NULL) AND :onDate  BETWEEN s.established AND s.dueTo ORDER BY s.established").setParameter("onDate", today).getResultList();
+        return em.createQuery("SELECT s from ServiceAllocation s WHERE s.employee.active IS TRUE AND s.active IS FALSE AND (s.deleted IS FALSE OR s.deleted IS NULL) AND (s.autoCanceled IS FALSE OR s.autoCanceled IS NULL) AND :onDate  BETWEEN s.established AND s.dueTo ORDER BY s.established").setParameter("onDate", today).getResultList();
     }
 	
 	@Asynchronous
@@ -58,16 +58,14 @@ public class ServiceAllocationActivactionProcessor extends BaseDatabaseAwareSeam
             @IntervalDuration Long interval,
             @FinalExpiration Date endDate) {
 	    Date today = new Date();
-	    info("will check for service allocations that should be set current on #0", today);
+	    info("will check for service allocations that should be set activated on #0", today);
 	    Collection<ServiceAllocation> serviceAllocationsThatShouldBeActivated = getServiceAllocationThatShouldBeActivated(getEntityManager(), today);
-	    info("found totally #0 disposals that should be activated", serviceAllocationsThatShouldBeActivated.size());
+	    info("found totally #0 activated that should be activated", serviceAllocationsThatShouldBeActivated.size());
 	    for(ServiceAllocation serviceAllocation : serviceAllocationsThatShouldBeActivated) {
 	        Employee employee = serviceAllocation.getEmployee();
 	        info("setting service allocation #0 as active for employee #1", serviceAllocation, employee);
-	        //serviceAllocation.setActive(Boolean.TRUE);
+	        serviceAllocation.setActive(Boolean.TRUE);
 	        
-	        //employee.setLeave(newCurrentLeave);
-	        //newCurrentLeave.setActive(Boolean.TRUE);
 	    }
 	    getEntityManager().flush();
 	    return null;
